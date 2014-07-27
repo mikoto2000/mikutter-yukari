8144e5c0665ff1c5c646b5f66d28c2b7663938aa
mikoto2000 <mikoto2000@gmail.com>
2014-07-27 23:44:48 +0900

    Added 'home timelone auto read mechanism'.

--- yukari.rb
+++ yukari.rb
@@ -2,15 +2,26 @@
 
 require "open3"
 require "fileutils"
+require "workers"
 
 Plugin.create(:yukari) do
-  # デフォルト値設定
+  # 最後に読んだツイートの時間を記憶
+  last_read_date = nil
+  # スレッドプール
+  pool = nil
+
   on_boot do |service|
+    # 設定のデフォルト値設定
     UserConfig[:yukari_create_command] ||= "~/bin/voiceroidCaller"
     UserConfig[:yukari_read_command] ||= "mpg123"
     UserConfig[:yukari_read_command_args] ||= ""
     UserConfig[:yukari_nhk_news_scrap_command] ||= "~/bin/scrapNhkNews"
     UserConfig[:yukari_working_directory] ||= "~/tmp"
+    UserConfig[:yukari_is_auto_read] ||= false
+
+    # 受信ツイート読み上げ機能初期化
+    last_read_date = Time.now
+    pool = Workers::Pool.new(:size => 1)
   end
 
   # config に設定項目を追加
@@ -21,6 +32,26 @@ Plugin.create(:yukari) do
           input '音声再生コマンド引数', :yukari_read_command_args
           input 'nhk_news スクレイピングコマンド', :yukari_nhk_news_scrap_command
           input '作業ディレクトリ', :yukari_working_directory
+      end
+      settings("自動読み上げ設定") do
+          boolean '自動読み上げを有効にする', :yukari_is_auto_read
+      end
+  end
+
+  # ツイート受信で自動読み上げ
+  onupdate do |service, messages|
+      # 受信したツイートを巡る
+      for message in messages do
+          # 起動以前のツイートは無視する
+          if message[:created] < last_read_date then
+              next
+          end
+
+          pool.perform do
+              if UserConfig[:yukari_is_auto_read] then
+                  readMessage(message)
+              end
+          end
       end
   end
 
@@ -34,20 +65,32 @@ Plugin.create(:yukari) do
     condition: Plugin::Command[:HasMessage],
     visible: true,
     role: :timeline) do |opt|
-        Thread.fork {
+        pool.perform do
             for message in opt.messages do
-                if message.user === 'nhk_news' then
-                    urls = URI.extract(message.to_s)
-                    for url in urls do
-                        read_string, e, s = Open3.capture3("#{UserConfig[:yukari_nhk_news_scrap_command]} #{url}")
-                        read(read_string)
-                    end
-                else
-                    read_string = message.to_s
-                    read(read_string)
-                end
+                readMessage(message)
             end
-        }
+        end
+    end
+
+    # メッセージを読み上げる
+    def readMessage(message)
+        read_string = getReadString(message)
+        read(read_string)
+    end
+
+    # メッセージから読み上げる文字列を取得する
+    def getReadString(message)
+       read_string = nil
+       if message.user === 'nhk_news' then
+           urls = URI.extract(message.to_s)
+           for url in urls do
+               read_string, _, _ = Open3.capture3("#{UserConfig[:yukari_nhk_news_scrap_command]} #{url}")
+           end
+       else
+           read_string = message.to_s
+       end
+
+       return read_string
     end
 
     # 使用するコマンド組立
@@ -57,7 +100,7 @@ Plugin.create(:yukari) do
     # 作業ディレクトリ・一時ファイルパス組立
     WORKING_DIR = UserConfig[:yukari_working_directory]
 
-
+    # read_string を読み上げる
     def read(read_string)
         tmp_voice_name = "#{WORKING_DIR}/yukari_#{Time.now.strftime('%Y%m%d%H%M%S')}"
         create = "#{YUKARI_COMMAND} #{tmp_voice_name} '#{read_string}'"
